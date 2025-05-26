1646b9ec9188c488ac15b9e9a8bd26bfcfbf93a2
satokenn <odinn3103@gmail.com>
2025-05-26 12:50:46 +0000

    update : 実験第3のレポートを更新 (第九回の方法を記述)

--- github/Report/pl3/code/Exp3_9_Matlab.m
+++ github/Report/pl3/code/Exp3_9_Matlab.m
@@ -26,23 +26,52 @@ for i = 1:num_trials
     expos(:, 1) = times_to_zero;
     expos(:, 2:4) = -1;
     
+    %expos の時間列を「終了時0基準にした」eye_data の時間と比較
     [is_valid_time, idx_in_eye_data] = ismember(expos(:, 1), eye_data(:, 1) - eye_data(end, 1));
+    
+    % is_valid_time は expos の時間が eye_data の時間に存在するかどうかを示す論理配列
+    % idx_in_eye_data は expos の時間が eye_data の時間に対応するインデックスを示す
     valid_rows = is_valid_time;
+    
+    % 欠損でない行に画面状態を代入に画面状態を代入
     expos(valid_rows, 2) = eye_data(idx_in_eye_data(valid_rows),3);
+    
+    % 欠損でない行に X座標を代入
+    % 少数の値になるため 10倍して整数に変換
     expos(valid_rows, 5) = round(eye_data(idx_in_eye_data(valid_rows), 4)*10);
+    
+    % 画面の状態が 1 の時の X座標を抽出
     filation_rows =  expos(:, 2) == 1 & expos(:, 5) ~= -1;
     fixation_x_values = expos(filation_rows, 5);
+    
+    % X座標の平均を算出し，中心座標とする
     center_x = round(mean(fixation_x_values));
+    
+    % 画像提示時のうち，X座標がcenter_x より小さい行を 100，それより大きい行を 102 とする
     left_rows = expos(:, 2) == 2 & expos(:,5) < center_x;
     expos(left_rows, 3) = 100;
+
     right_rows = expos(:, 2) == 2 & expos(:,5) > center_x;
     expos(right_rows, 3) = 102;
+
+    % どちらの画像が魅力的だったかを取得
     selected_side(i) = data(i,2);
+
+    % 欠損値でないデータを探す
     not_missing = expos(:, 3) ~= -1;
+
+    % キー出力データファイルと比較して，一致していれば 1 を出力
     expos(not_missing & expos(:, 3) == selected_side(i), 4)  = 1;
     
+    % 一致していない場合と欠損値の場合は 0 を出力
     expos(not_missing & expos(:, 3) ~= selected_side(i), 4) = 0;
+
+    % 魅力的だと判断した画像の方向と見ている方向が一致しているかのデータを
+    % キー押し時の 1 秒前からキー押し時まで取得し，対応している試行の行に値を代入
+    % ここではキー押し時を 0 として，-998 ms から 0 ms の間のデータを取得
     time_window = expos(:,1) >= -998 & expos(:,1) <= 0;
+    
+    % 該当する expos 4 列目を抽出して代入
     selected_segment = expos(time_window, 4);
     data_matrix(:, i + 1) = selected_segment;
 end
